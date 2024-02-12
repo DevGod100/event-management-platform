6e76f60970c6ec62c52a684426a515850f0d35d7
Steve Loewenstein <saloewenstein@my365.bellevue.edu>
2024-02-12 15:40:52 +0200

    midevents form

---
+++ components/shared/FileUploader.tsx
@@ -0,0 +1,15 @@
+import { Dispatch, SetStateAction } from "react"
+
+type FileUploadProps = {  
+    imageUrl: string
+onFieldChange:(value: string) => void
+setFiles: Dispatch<SetStateAction<File[]>>
+}
+
+const FileUploader = ({onFieldChange, imageUrl, setFiles}: FileUploadProps) => {
+  return (
+    <div>FileUploader</div>
+  )
+}
+
+export default FileUploader
