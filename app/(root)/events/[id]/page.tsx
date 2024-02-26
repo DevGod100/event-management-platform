2239c91e9c4c51958955aa73dbc49f6d75066270
Steve Loewenstein <saloewenstein@my365.bellevue.edu>
2024-02-26 16:09:32 +0200

    reached "profile page" section

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,10 +1,17 @@
-import { getEventById } from "@/lib/actions/event.actions";
+import Collection from "@/components/shared/Collection";
+import { getEventById, getRelatedEventsByCategory } from "@/lib/actions/event.actions";
 import { formatDateTime } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
 
-const EventDetails = async ({ params: { id } }: SearchParamProps) => {
+const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) => {
   const event = await getEventById(id);
+
+  const relatedEvents = await getRelatedEventsByCategory({
+    categoryId: event.category._id,
+    eventId: event._id,
+    page: searchParams.page as string
+  })
 
   return (
     <>
@@ -71,7 +78,8 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
       </div>
     </section>
 
-    {/* EVENTS with the same category 
+    {/* EVENTS with the same category  */}
+    
       <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
       <h2 className="h2-bold">Related Events</h2>
 
@@ -84,7 +92,7 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
           page={searchParams.page as string}
           totalPages={relatedEvents?.totalPages}
         />
-    </section> */}
+    </section>
     </>
   );
 };
